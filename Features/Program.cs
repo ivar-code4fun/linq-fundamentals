dfbe220c5a30c8e7906921df791c0526a54d4623
ravishankar <ravishankar.r1@gmail.com>
2017-06-24 00:48:08 -0700

    Linq features

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,16 +10,32 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            IEnumerableDemo();
-            //UnderstandingLambdaExpressions();
+            //FuncAndAction();
 
+            //IEnumerableDemo();
+            UnderstandingLambdaExpressions();
+
+        }
+
+        private static void FuncAndAction()
+        {
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(square(add(3, 5)));
+
+            Action<int> write = x => Console.WriteLine(x);
+            write(square(add(7, 3)));
         }
 
         private static void UnderstandingLambdaExpressions()
         {
-            var cities = new List<string>(){"London", "Paris", "Seattle", "Lisbon"};
+            var cities = new List<string>(){"Boston","Los Angeles","London","Seattle","Hyderabad"};
 
-            IEnumerable<string> filteredCities = cities.Where(city => city.StartsWith("L"));
+            //IEnumerable<string> filteredCities = cities.Where(city => city.StartsWith("L"));
+            IEnumerable<string> filteredCities = from city in cities
+                                                 where city.StartsWith("L") && city.Length < 8
+                                                 orderby city
+                                                 select city;
 
             foreach (var city in filteredCities)
             {
@@ -35,6 +51,7 @@ namespace Features
                 new Employee { Id = 1, Name = "Scott" },
                 new Employee { Id = 2, Name = "Chris" }
                         };
+
             Console.WriteLine("Number of developers : {0}", developers.Count());
 
             IEnumerable<Employee> sales = new List<Employee>()
@@ -50,7 +67,12 @@ namespace Features
             //    Console.WriteLine(enumerator.Current.Name);
             //}
 
-            foreach (var employee in developers.Where(dev => dev.Name.StartsWith("S")))
+            //foreach (var employee in developers.Where(dev => dev.Name.StartsWith("S")))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            foreach (var employee in developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name))
             {
                 Console.WriteLine(employee.Name);
             }
