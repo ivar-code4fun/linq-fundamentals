e88b3337147b69619c846eeacb331bceca7fd610
ravishankar <ravishankar.r1@gmail.com>
2017-07-05 21:41:23 -0700

    Fuel efficient car test using Linq

---
+++ Cars/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            //var query = cars.OrderByDescending(c => c.Combined)
+            //                .ThenBy(c => c.Name);
+            var query = from car in cars
+                        where car.Manufacturer == "BMW" && car.Year == 2016
+                        orderby car.Combined descending, car.Name
+                        select car;
+
+            var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .Select(c => c)
+                            .First();
+
+            Console.WriteLine(top.Name);
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+            }
+
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            var query = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Car.ParseFromCsv);
+
+            return query.ToList();
+        }
+
+
+
+    }
+}
